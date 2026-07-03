3f6177fdd0640edace448bfe369a52fefff400a1
apososgol1000-png <apososgol1000@gmail.com>
2026-07-03 15:53:12 -0600

    Add Client struct and Send method for IPC

--- internal/ipc/client.go
+++ internal/ipc/client.go
@@ -1,3 +1,25 @@
+package ipc
+
+import (
+    "encoding/json"
+    "net"
+    "time"
+)
+
 type Client struct {
-    conn net.Conn
+    path string
+}
+
+func NewClient(path string) *Client {
+    return &Client{path: path}
+}
+
+func (c *Client) Send(req Request) error {
+    conn, err := net.DialTimeout("unix", c.path, 500*time.Millisecond)
+    if err != nil {
+        return err
+    }
+    defer conn.Close()
+    enc := json.NewEncoder(conn)
+    return enc.Encode(req)
 }
