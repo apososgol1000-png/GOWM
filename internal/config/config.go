93a58d5c5b96f253cfd62e62e4e34020e71b7868
apososgol1000-png <apososgol1000@gmail.com>
2026-07-03 15:50:25 -0600

    Add JSON tags to Config struct fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
+package config
+
 type Config struct {
-    BorderWidth int
-    Gaps int
-    FocusColor string
-    Terminal string
+    BorderWidth int    `json:"border_width"`
+    Gaps        int    `json:"gaps"`
+    FocusColor  string `json:"focus_color"`
+    Terminal    string `json:"terminal"`
 }
